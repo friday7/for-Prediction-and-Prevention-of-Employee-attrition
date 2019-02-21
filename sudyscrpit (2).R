33be3f23630678b7690b061e3d9ad3cf53e44414
Samridh Sharma <44182152+friday7@users.noreply.github.com>
2019-02-21 14:18:22 +0000

    R script

---
+++ sudyscrpit (2).R
@@ -0,0 +1,28 @@
+setwd('/Users/Raghav/Downloads')
+#####overall business module of tedx
+
+library(caret)
+library(e1071)
+set.seed(21112)
+
+sudy = read.csv("sudy.csv",stringsAsFactors = TRUE)
+sudy = sudy[,-c(1,3,5,7,11)]
+sudy = sudy[,-c(9)]
+
+str(sudy)
+sudy$Satisfied_y.n = factor(sudy$Satisfied_y.n)
+sudy$Project...4 = factor(sudy$Project...4)
+sudy$Monthly.hour...190 = factor(sudy$Monthly.hour...190)
+sudy$Time_spend_over.5 = factor(sudy$Time_spend_over.5)
+sudy$Work_accident = factor(sudy$Work_accident)
+sudy$left = factor(sudy$left)
+str(sudy)
+
+index = sample(1:dim(sudy)[1],dim(sudy)[1]*0.8, replace=FALSE)
+train = sudy[index,]
+test = sudy[-index,]
+
+nb = naiveBayes(left~.,train$left)
+nbpredict = predict(nb,test[,-c(6)])
+levels(sudy$left)
+confusionMatrix(nbpredict,test$left,positive = "1")
